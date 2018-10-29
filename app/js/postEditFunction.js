e022c44b367af60e9986f855151701db68de4dcd
Matt <matthew.tapley@hotmail.co.uk>
2018-10-29 16:18:52 +0000

    created function to get edit user input into a function, modified post edit user info to api

--- app/js/postEditFunction.js
+++ app/js/postEditFunction.js
@@ -1,12 +1,32 @@
-// get the user object from the input in the modal
-// sending the data to the api
+/*
+    Creates an object of the user from the data in the input fields
+    Should be called within the submit event listener
 
-//
+    @param a string in the '.class' format, the class on each of the input fields of the edit user form
 
+    @return Returns an object that can be used as the parameter in postUserData() func.
+ */
 
-function postUserEdit(data) {
-    let editedUser = fetch('localhost:8080/user/edit', {
-        method: 'POST',
-        body: JSON.stringify(new editedUser(document.getElementById('editUserForm').value))
+async function createUserObject(inputClass) {
+    let formData = document.querySelectorAll(inputClass)
+    let result = {}
+    formData.forEach(function(input) {
+        result[input.name] = input.value
     })
+    return result
+}
+
+/*
+    Posts edited user data to the API
+
+    @param the formData is the object, use the returned result from createUserObject
+ */
+
+async function postUserEdit(formData) {
+    if (formData.name && formData.email && formData.id) {
+        await fetch('localhost:8080/user/edit', {
+            method: 'POST',
+            body: JSON.stringify(formData)
+        })
+    }
 }
