76135159c47622bd2f6bd1cf292597d5728b63da
Imogen <imogenhallett@hotmail.com>
2018-10-29 15:53:20 +0000

    Made previous & next function generic so can load any question

--- app/js/questionNav.js
+++ app/js/questionNav.js
@@ -1,54 +1,62 @@
 var current = 1
 
 /**
-*adds active to the first question
-* function in called when questions are inserted into html
+ * adds active to the first question
+ * function in called when questions are inserted into html
  */
 function active() {
+    let questionCount = document.querySelectorAll('#questions .question').length
     document.querySelector(".q_" + current).classList.add("active")
-    document.querySelector("h4").textContent = current + "/30"
+    document.querySelector("h4").textContent = current + "/" + questionCount
 }
 
 /**
-*adds active class to next question, removes from current question
-* makes the prev and next buttons appear when needed
+ * adds active class to next question, removes from current question
+ * makes the prev and next buttons appear when needed
  */
 function next() {
     current++
-    let nextQuestion = document.querySelector(".q_" + current)
-    document.querySelector("h4").textContent = current + "/30"
-
-    if (current !== 1) {
-        document.querySelector(".prev").style.display = "block"
-    }
-    if (current === 30) {
-        document.querySelector(".next").style.display = "none"
-    }
-    if (nextQuestion !== null) {
-        document.querySelector(".active").classList.remove("active")
-        document.querySelector(".q_" + current).classList.add("active")
-    }
+    changeQuestion(current)
 }
 
 /**
-*adds active class to prev question, removes from current question
-* makes the prev and next buttons appear when needed
+ * adds active class to prev question, removes from current question
+ * makes the prev and next buttons appear when needed
  */
 function prev() {
     current--
-    let prevQuestion = document.querySelector(".q_" + current)
-    document.querySelector("h4").textContent = current + "/30"
+    changeQuestion(current)
+}
 
-    if (current === 1) {
-        document.querySelector(".prev").style.display = "none"
-        document.querySelector(".next").style.display = "block"
+/**
+ * Takes you to any question based on parameter
+ *
+ * @param destinationPage is question to load
+ */
+function changeQuestion(destinationPage) {
+    let destinationQuestion = document.querySelector(".q_" + destinationPage)
+    let questionCount = document.querySelectorAll('#questions .question').length
+    let nextButton = document.querySelector(".next")
+    let prevButton = document.querySelector(".prev")
+    document.querySelector("h4").textContent = destinationPage + "/" + questionCount
+
+    switch(destinationPage) {
+        case 1:
+            prevButton.style.display = "none"
+            nextButton.style.display = "block"
+            break;
+        case questionCount:
+            prevButton.style.display = "block"
+            nextButton.style.display = "none"
+            break;
+        default:
+            prevButton.style.display = "block"
+            nextButton.style.display = "block"
     }
-    if (current !== 30) {
-        document.querySelector(".next").style.display = "block"
-    }
-    if (prevQuestion !== null) {
-        document.querySelector(".active").classList.remove("active")
-        document.querySelector(".q_" + current).classList.add("active")
+
+    if (destinationQuestion !== null) {
+        document.querySelector("#questions .active").classList.remove("active")
+        destinationQuestion.classList.add("active")
     }
 }
 
