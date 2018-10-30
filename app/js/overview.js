53b1cabb1efe295ed77bcd546809509696b639e8
Burak Tilek <burak.tilek@gmail.com>
2018-10-30 14:35:35 +0000

    added icons to template to overview

--- app/js/overview.js
+++ app/js/overview.js
@@ -4,8 +4,8 @@ document.querySelector("#finish").addEventListener("click", function() {
     for (let i = 1; i <= numOfQuestions; i++) {
         let questionObject = {}
         questionObject['id'] = i
-        questionObject['isFlagged'] = Object.values(flaggedQuestions)[i] ? 'Yes' : 'No'
-        questionObject['isAnswered'] = getUserAnswers()[i] == 'unanswered' ? 'No' : 'Yes'
+        questionObject['isFlagged'] = flaggedQuestions[i]
+        questionObject['isAnswered'] = getUserAnswers()[i] == 'unanswered' ? false : true
         questionObject['question'] = document.querySelectorAll(`.q_${i} p`)[1].innerText.substring(0, 20) + '...'
         overviewData.push(questionObject)
     }
