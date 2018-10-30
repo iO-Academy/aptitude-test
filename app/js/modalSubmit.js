e7681c69c990ed753c7efd36976f7af5f893ba0d
Ariel Walker-Cochrane <arielwalkercochrane@gmail.com>
2018-10-30 15:39:01 +0000

    removed unwanted file

--- app/js/modalSubmit.js
+++
@@ -1,7 +0,0 @@
-
-var modal = document.getElementsByClassName("modal")
-
-document.getElementsByClassName("submit").addEventListener('click', function () {
-    //apiPostFunction(edited user data)
-    modal.style.display = "none"
-    })
