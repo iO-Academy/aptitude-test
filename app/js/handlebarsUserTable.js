3260f9a67a44e78990f3cf503545a2371d1e55a3
charliecog <charlie.coggans@mayden.co.uk>
2018-10-30 11:20:22 +0000

    abstracted out excessive logic from fillUserTable(), amended delete function to select element dynamically rather than pass it in as param

--- app/js/handlebarsUserTable.js
+++ app/js/handlebarsUserTable.js
@@ -11,28 +11,34 @@ function fillUserTable(HBTemplate) {
             return result.json()
         })
         .then(function(result) {
-            let user_list = document.querySelector(".user_list")
-            user_list.innerHTML = ""
+            updateDisplayedUsers(result, template)
+        })
+}
 
-            if (result.success) {
-                result.data.forEach(function(userData) {
-                    if (userData.deleted === "0") {
-                        let html = template(userData)
-                        user_list.innerHTML += html
-                    }
-                })
-                let userItems = document.querySelectorAll(".user_item")
-                userItems.forEach(function (userItem) {
-                    let deleteButton = userItem.querySelector('.btn-danger')
-                    deleteButton.addEventListener('click', function () {
-                        let userId = userItem.getAttribute("dataId")
-                        deleteUser(userId, userItem)
-                    })
-                })
-            } else {
-                user_list.innerHTML = "Please contact Admin, user list unavailable"
+function updateDisplayedUsers(apiResponse, template) {
+    let user_list = document.querySelector(".user_list")
+    user_list.innerHTML = ""
+    if (apiResponse.success) {
+        apiResponse.data.forEach(function(userData) {
+            if (userData.deleted === "0") {
+                let html = template(userData)
+                user_list.innerHTML += html
             }
         })
+        addDeleteEventListeners()
+    } else {
+        user_list.innerHTML = "Please contact Admin, user list unavailable"
+    }
+}
+
+function addDeleteEventListeners() {
+    let userItems = document.querySelectorAll(".btn-danger")
+    userItems.forEach(function (userItem) {
+        userItem.addEventListener('click', function (e) {
+            let userId = e.target.parentElement.getAttribute("dataId")
+            deleteUser(userId)
+        })
+    })
 }
 
 /**
@@ -48,12 +54,11 @@ function updateUserTable() {
 // it then posts the delete line and changes the entry in the database from a zero to a one.
 //if succesful the function deletes teh innerHTML of the parent element (as if it is actually deleted) and console logs
 // deletion succesful.
-function deleteUser(userId, userEntry) {
+function deleteUser(userId) {
     let url = "http://localhost:8080/user/delete/" + userId
     fetch(url, {"method": "post"})
         .then(function () {
-            userEntry.innerHTML = ""
-            console.log("You have succesfully soft deleted the entry with the id of " + userId)
+            updateUserTable()
         })
 }
 
