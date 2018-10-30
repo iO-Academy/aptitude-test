f615f2c37d68a9953e0f59c0108aeafc2c27299a
Gideon Spektor <gidspektor@gmail.com>
2018-10-30 09:59:31 +0000

    took away the post function and just added in a function that makes the modal delete itself after the submit button is pressed

--- app/js/modalSubmit.js
+++ app/js/modalSubmit.js
@@ -2,6 +2,6 @@
 var modal = document.getElementsByClassName("modal")
 
 document.getElementsByClassName("submit").addEventListener('click', function () {
-
+    //apiPostFunction(edited user data)
     modal.style.display = "none"
-})
+    })
