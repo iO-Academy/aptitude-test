500de5719f1d6d5d99b3b85892d2496fd74bf86d
Daniel <hoptond848@protonmail.com>
2018-10-30 14:30:26 +0000

    remove console.log

--- app/js/questions.js
+++ app/js/questions.js
@@ -36,6 +36,5 @@ getTemplateAjax('js/templates/questions.hbs').then(function(HBTemplate) {
 
 document.querySelector('#flag-checkbox').addEventListener('change', function() {
     let qid  = document.querySelector('.question.active').dataset.id
-    console.log(qid)
     flaggedQuestions[qid] = document.querySelector('#flag-checkbox').checked
 })
