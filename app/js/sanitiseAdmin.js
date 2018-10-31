4dc0577accf884c9afed0f80c5c82652f179f1ac
Matt <matthew.tapley@hotmail.co.uk>
2018-10-31 14:10:12 +0000

    edited docBlocks in sanitiseAdmin.js to include params and resolve typos

--- app/js/sanitiseAdmin.js
+++ app/js/sanitiseAdmin.js
@@ -2,7 +2,9 @@
  * Add in isEmailValid from validateUserAdditions to validate edit email.
  * To make sure name and email fields aren't empty
  *
- * @return Will return false else will return true depending if the the filed is typed.
+ * @param field is the input field being inspected
+ *
+ * @return Will return false else will return true depending if the the field is typed.
  */
 function isEmpty (field) {
     if (field.value === "") {
@@ -15,11 +17,13 @@ function isEmpty (field) {
 /**
  * To make sure numbers or special characters aren't in the name field.
  *
- * @return Will return true if filed has been validated with correct characters else will return false.
+ * @param field is the input field being inspected
+ *
+ * @return Will return true if field has been validated with correct characters else will return false.
  */
 function nameValidation (field) {
     var regexLetters = /[A-Za-z]+$/
-    if(field.match(regexLetters) ) {
+    if (field.match(regexLetters) ) {
         return true
     } else {
         return false
