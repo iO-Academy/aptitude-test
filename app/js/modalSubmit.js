2092468471a7bec5ece0b6d429619b9639bc39ff
Matt <matthew.tapley@hotmail.co.uk>
2018-10-31 15:05:04 +0000

    added original email hidden input to editmodal.hbs, resolved validation issue with addEditModalSubmitEventListener() func.

--- app/js/modalSubmit.js
+++ app/js/modalSubmit.js
@@ -1,21 +1,26 @@
 /**
- * An event listener on the submit button, sends the object values through our validation functions.
- * If all returned as true, modal closes, the data is stored in the database and the table on the frontend is updated.
+ * this is an event listener on the submit button (listens for click), it sends the object values through our validation functions.
+ * If email exists in db then submit is not allowed. If original email matches current or email does not exist is db then user data is updated.
  *
  */
+
 function addEditModalSubmitEventListener() {
     document.getElementById('editSubmit').addEventListener('click', function() {
-        let name = document.getElementById('firstName').value
-        let email = document.getElementById('email').value
-        if( isEmpty(name) &&
-            isEmpty(email) &&
-            nameValidation(name) &&
-            isEmailValid(email)
-            ){
-            closeDialog()
-            postUserEdit(createObjectForDatabase('.editUserData'))
-            updateUserTable()
-        }
+        let name = document.getElementById("firstName").value
+        let email = document.getElementById("email").value
+        let originalEmail = document.getElementById("originalEmail").value
+
+        getExistingUsers().then(function (existingUsers) {
+            if (isEmpty(name) &&
+                isEmpty(email) &&
+                nameValidation(name) &&
+                isEmailValid(email)) {
+                if(originalEmail == email || userExists(email, existingUsers) == false){
+                    closeDialog()
+                    postUserEdit(createUserObject('.editUserData'))
+                    updateUserTable()
+                }
+            }
+        })
     })
 }
-
