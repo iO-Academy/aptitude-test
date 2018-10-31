f431862a3353de60d7ebcc4c1798506aecd76f44
Daniel Coombes <daniel.coombes224@gmail.com>
2018-10-31 12:16:15 +0000

    Made some spelling corrections and changed to the appropriate doc block style, also added some more line breaks

--- app/js/modalSubmit.js
+++ app/js/modalSubmit.js
@@ -1,8 +1,8 @@
 /**
  * An event listener on the submit button, sends the object values through our validation functions.
  * If all returned as true, modal closes, the data is stored in the database and the table on the frontend is updated.
+ *
  */
-
 function addEditModalSubmitEventListener() {
     document.getElementById('editSubmit').addEventListener('click', function() {
         let name = document.getElementById('firstName').value
