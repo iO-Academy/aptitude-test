7282c7c923c883b922ff0b44109b6e4d58a7b20a
olivergate <oliver.kg2@gmail.com>
2018-11-01 16:44:30 +0000

    function completely rewritten because apparently they want only a score table...

--- app/js/handlebarsScoreTable.js
+++ app/js/handlebarsScoreTable.js
@@ -13,23 +13,45 @@ function updateScoreTable() {
 }
 
 /**
- * fills handlebars template by passing in object and inserts into the score_list div
+ * fills handlebars template by passing in object and inserts into the score_list div and checks for deleted
+ * in database
  *
  * @param HBTemplate the handlebars template
  * @param ObjFunction the function that creates an object of all fields required in scores page
  */
 function fillScoreTable(HBTemplate, userInfo) {
-    let template = Handlebars.compile(HBTemplate)
-    let score_list = document.querySelector(".score_list")
+    fetch("http://localhost:8080/user")
+        .then(function(result) {
+            return result.json()
+        })
+        .then(function(result){
+            let newResultObject = []
+            result.data.forEach(function (existingUser) {
+                if (existingUser.deleted != 1){
+                    userInfo.data.forEach(function (scoreUser) {
+                        if (scoreUser.email === existingUser.email) {
+                            newResultObject.push(scoreUser)
+                        }
+                    })
+                }
+            })
+           return ({data: newResultObject})
+        }).then(function (finalObject) {
+            console.log(finalObject)
+        let template = Handlebars.compile(HBTemplate)
+        let score_list = document.querySelector(".score_list")
 
-    score_list.innerHTML = ""
+        score_list.innerHTML = ""
 
-    if (userInfo.success === true) {
-        let html = template(userInfo)
-        score_list.innerHTML += html
-    } else {
-        score_list.innerHTML = "Please contact Admin, user list unavailable"
-    }
+        if (userInfo.success === true) {
+            let html = template(finalObject)
+            score_list.innerHTML += html
+        } else {
+            score_list.innerHTML = "Please contact Admin, user list unavailable"
+        }
+    })
+
+
 }
 
 updateScoreTable()
