4d1e82a7432bd208aa92f3bf346464b717a5bedb
Felix <felixgate13@gmail.com>
2018-11-01 14:29:56 +0000

    made the logo chill at the bottom made the arrw flippy and changed button

--- app/js/admin-page.js
+++ app/js/admin-page.js
@@ -1,7 +1,13 @@
 
-
+var clicked = 0
 
 $('#drop-down').on('click', function() {
     $('#addNewUserForm').collapse('toggle')
-
+    clicked++
+    if (clicked%2 == 0 || clicked == 0) {
+        document.querySelector('#drop-down').innerHTML ="Add New User <span>&#x2193;</span>"
+    }
+    else {
+        document.querySelector('#drop-down').innerHTML = "Add New User <span>&#x2191;</span>"
+    }
 })
