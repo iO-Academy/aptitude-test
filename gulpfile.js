3918f113e18d9230ec1f25a4e0296d243cba0b5c
Mike Oram <mike.oram@mayden.co.uk>
2019-04-15 09:38:34 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
 
-gulp.task('sass', function() {
+gulp.task('sass', function(done) {
     // Gets all files ending with .scss in app/scss
     return gulp.src(['app/scss/styles.scss', 'app/scss/questionPage.scss', 'app/scss/resultPage.scss'])
         .pipe(sass())
         .pipe(gulp.dest('app/css'))
+    done()
 })
 
-gulp.task('watch', ['sass'], function (){
-    gulp.watch('app/scss/**/*.scss', ['sass'])
+gulp.task('watch', ['sass'], function (done){
+    gulp.watch('app/scss/**/*.scss', gulp.series('sass'))
+    done()
 })
