79efc2866c32135216f5f0d1e2cce45851b50d17
Alessandro Corra <alecorra1980@gmail.com>
2019-04-15 16:52:13 +0100

    event listener added on submit button, form data into variables

---
+++ app/js/validateQuestionsFormData.js
@@ -0,0 +1,11 @@
+document.querySelector('.add_question').addEventListener('submit', function(event) {
+    event.preventDefault()
+    let question = document.getElementById('question').value
+    let answer1 = document.getElementById('answer1').value
+    let answer2 = document.getElementById('answer2').value
+    let answer3 = document.getElementById('answer3').value
+    let answer4 = document.getElementById('answer4').value
+    let answer5 = document.getElementById('answer5').value
+
+
+})
