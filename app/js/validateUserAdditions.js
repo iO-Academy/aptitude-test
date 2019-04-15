d7c106d5cd73715412284afeb9b202564504f81f
Liliya Voevodina <liliiavoevodina@gmail.com>
2019-04-15 15:51:02 +0100

    Validated a time input so its not zero ro null or empty, Made 30 a default, Made is to be sent to JSOn timed 60 so its seconds in a DB ( not minutes)

--- app/js/validateUserAdditions.js
+++ app/js/validateUserAdditions.js
@@ -85,18 +85,22 @@ document.querySelector('#addNewUserForm').addEventListener('submit', function(ev
     var emailField = document.getElementById("email")
     var nameField = document.getElementById('name')
     var errorField = document.getElementById('error')
+    var errorFieldTime = document.getElementById('error')
+    var timeField = document.getElementById('time')
 
     getExistingUsers().then(function(existingUsers) {
         if (!isEmailValid(emailField.value) || userExists(emailField.value, existingUsers)) {
-            var errorMessage = "Your email is not valid or already exists: Please provide a correct email"
-            errorField.innerHTML = errorMessage
+            errorField.innerHTML = "Your email is not valid or already exists: Please provide a correct email"
+        } else if (timeField.value <=0 || timeField.value == null || typeof timeField.value !== 'number') {
+            errorFieldTime.innerHTML = 'This is not a good number!'
         } else {
             errorField.innerHTML = ''
-
-            saveNewUser({'name': nameField.value, 'email': emailField.value}).then(function(response) {
+            var setTime = timeField.value * 60
+            saveNewUser({'name': nameField.value, 'email': emailField.value, 'time': setTime}).then(function(response) {
                 if (response.success) {
                     nameField.value = ''
                     emailField.value = ''
+                    timeField.value = 30
                     updateUserTable()
                 } else {
                     errorField.innerHTML = response.message
