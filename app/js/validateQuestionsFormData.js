e783232b0da9ddcd9a2de41877fbf64c09a95e7c
Alessandro Corra <alecorra1980@gmail.com>
2019-04-15 17:02:05 +0100

    function isEmpty and if statement created

--- app/js/validateQuestionsFormData.js
+++ app/js/validateQuestionsFormData.js
@@ -1,3 +1,13 @@
+function isEmpty(value) {
+    if (!value && value !== 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
 document.querySelector('.add_question').addEventListener('submit', function(event) {
     event.preventDefault()
     let question = document.getElementById('question').value
@@ -6,6 +16,12 @@ document.querySelector('.add_question').addEventListener('submit', function(even
     let answer3 = document.getElementById('answer3').value
     let answer4 = document.getElementById('answer4').value
     let answer5 = document.getElementById('answer5').value
+
+    if (isEmpty(question)) {
+        console.log(isEmpty(question))
+    } else {
+        console.log(isEmpty(question))
+    }
 
 
 })
