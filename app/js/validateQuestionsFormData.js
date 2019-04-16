56ee7b23e794e8a04815d7cd73e8123b3cdd807d
Alessandro Corra <alecorra1980@gmail.com>
2019-04-16 11:37:35 +0100

    form validation for question and minimum first 2 answers

--- app/js/validateQuestionsFormData.js
+++ app/js/validateQuestionsFormData.js
@@ -6,22 +6,52 @@ function isEmpty(value) {
     }
 }
 
-
-
 document.querySelector('.add_question').addEventListener('submit', function(event) {
     event.preventDefault()
-    let question = document.getElementById('question').value
-    let answer1 = document.getElementById('answer1').value
-    let answer2 = document.getElementById('answer2').value
-    let answer3 = document.getElementById('answer3').value
-    let answer4 = document.getElementById('answer4').value
-    let answer5 = document.getElementById('answer5').value
 
-    if (isEmpty(question)) {
-        console.log(isEmpty(question))
-    } else {
-        console.log(isEmpty(question))
+    let newQuestion = {
+        "text": question,
+        "option1": answer1,
+        "option2": answer2,
+        "option3": answer3,
+        "option4": answer4,
+        "option5": answer5,
+        // "answer": answer
     }
 
+    let answers = [answer1, answer2, answer3, answer4, answer5]
 
+    question = document.getElementById('question').value
+    answer1 = document.getElementById('answer1').value
+    answer2 = document.getElementById('answer2').value
+    answer3 = document.getElementById('answer3').value
+    answer4 = document.getElementById('answer4').value
+    answer5 = document.getElementById('answer5').value
+
+    if (!isEmpty(question)) {
+        newQuestion.text = question
+        document.querySelector('#question-error').classList.add('hidden')
+    } else {
+        document.querySelector('#question-error').classList.remove('hidden')
+    }
+
+    if (!isEmpty(answers[0]) && !isEmpty(answers[1])) {
+        newQuestion.option1 = answers[0]
+        newQuestion.option2 = answers[1]
+        document.querySelector('#answer1-error').classList.add('hidden')
+        document.querySelector('#answer2-error').classList.add('hidden')
+    } else if (isEmpty(answers[0]) && !isEmpty(answers[1])) {
+        document.querySelector('#answer1-error').classList.remove('hidden')
+        if (!document.querySelector('#answer2-error').classList.contains('hidden')) {
+            document.querySelector('#answer2-error').classList.add('hidden')
+        }
+    } else if (!isEmpty(answers[0]) && isEmpty(answers[1])) {
+        if (!document.querySelector('#answer1-error').classList.contains('hidden')) {
+            document.querySelector('#answer1-error').classList.add('hidden')
+        }
+        document.querySelector('#answer2-error').classList.remove('hidden')
+    } else {
+        document.querySelector('#answer1-error').classList.remove('hidden')
+        document.querySelector('#answer2-error').classList.remove('hidden')
+    }
 })
