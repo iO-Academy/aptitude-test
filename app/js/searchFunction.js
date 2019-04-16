970df9cf720b7477a87803c38b849aec014600e5
aldofiore28 <aldo.fiore95@gmail.com>
2019-04-16 10:30:13 +0100

    added function to validate and sanitize the input from the search box

--- app/js/searchFunction.js
+++ app/js/searchFunction.js
@@ -1,1 +1,27 @@
-let variable = 'test'
+/**
+ * Takes the input from the search box and checks if it is an illegal character
+ *
+ * @param input value from the search box
+ *
+ * @returns {boolean} true or false if the input is valid or not
+ */
+function validateInput (input) {
+    const checkValue = /^[\w-_.@ ]*$/
+    if (!input.match(checkValue)) {
+        return false
+    } else {
+        return true
+    }
+}
+
+/**
+ *
+ * removes all the whitespaces from the start and the end of the input
+ *
+ * @param input value from the search box
+ *
+ * @returns {string} string without whitespaces
+ */
+function sanitizeInput (input) {
+    return input.trim()
+}
