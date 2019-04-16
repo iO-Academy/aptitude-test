da68042bf23d0cc58bf36ccc4b3155bb96b9ff20
rossanderson75 <ross.anderson@mayden.co.uk>
2019-04-16 14:34:03 +0100

    added time input field to edit user modal and poulated time from API result and validated before save

--- app/js/handlebarsUserTable.js
+++ app/js/handlebarsUserTable.js
@@ -15,7 +15,6 @@ function fillUserTable(HBTemplate) {
             result.data.forEach(function(user){
                 user.time = user.time / 60
             })
-            console.log(result.data)
             updateDisplayedUsers(result, template)
         })
 }
@@ -59,7 +58,7 @@ function fillEditModal(HBTemplate, userInfo) {
 
     modal_content.innerHTML = ""
 
-    if (userInfo.name && userInfo.email && userInfo.id) {
+    if (userInfo.name && userInfo.email && userInfo.id && userInfo.time) {
         let html = template(userInfo)
         modal_content.innerHTML += html
     } else {
@@ -93,6 +92,7 @@ function createObjectFromParentElement(event) {
     userInfo.name = parentElement.getAttribute("dataName")
     userInfo.email = parentElement.getAttribute("dataEmail")
     userInfo.id = parentElement.getAttribute("dataId")
+    userInfo.time = parentElement.getAttribute("dataTime")
     userInfo.canRetake = parseInt(parentElement.getAttribute("dataCanRetake"))
     return userInfo
 }
--- app/js/modalSubmit.js
+++ app/js/modalSubmit.js
@@ -1,19 +1,46 @@
+
+
+// function isTimeValid (time) {
+//     if (!time <=1 || !time == null || isNaN(time) === false ) {
+//         return true
+//     }
+// }
+
+
+function isTimeValid(time) {
+    // console.log(typeof(time))
+    let intTime = parseInt(time)
+    console.log(typeof(intTime))
+
+    if (intTime <=1 || intTime == null) {
+
+        return false
+     } else {
+        	return true
+     }
+}
+
+
 /**
  * this is an event listener on the submit button (listens for click), it sends the object values through our validation functions.
  * If email exists in db then submit is not allowed. If original email matches current or email does not exist is db then user data is updated.
  *
  */
 
+
 function addEditModalSubmitEventListener() {
     document.getElementById('editSubmit').addEventListener('click', function() {
         let name = document.getElementById("firstName").value
         let email = document.getElementById("email").value
+        let time = document.getElementById('time').value
         let originalEmail = document.getElementById("originalEmail").value
-
         getExistingUsers().then(function (existingUsers) {
             if (isEmpty(name) &&
                 isEmpty(email) &&
                 nameValidation(name) &&
+                isTimeValid (time) &&
+
+
                 isEmailValid(email)) {
                 if(originalEmail == email || userExists(email, existingUsers) == false){
                     closeDialog()
