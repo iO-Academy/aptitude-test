dd99db68fcc47cfc4df47855b809f7535b9cec98
katestarks <katestarks@me.com>
2019-04-16 10:45:52 +0100

    first draft function

--- app/js/searchFunction.js
+++ app/js/searchFunction.js
@@ -1,1 +1,15 @@
-let variable = 'test'
+function search (copy) {
+    let newCopy = []
+    let searchInput = document.getElementById('searchForm').value
+    let regexSearch = new RegExp('/*' + searchInput + '*/i')
+    if (searchInput.length !== 0) {
+        copy.forEach(copy => {
+            if (regexSearch.test(copy.name) || regexSearch.test(copy.email)) {
+                newCopy.push(data)
+            }
+        })
+        return newCopy
+    }   else {
+        return copy
+    }
+}
