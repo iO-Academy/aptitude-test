26a4d7716bed6a296f92d92e03693d91c094785a
HazDuck <peter.e.heyes@gmail.com>
2019-04-17 09:37:32 +0100

    created eventlistener for score button

--- app/js/filterFunctions.js
+++ app/js/filterFunctions.js
@@ -1,22 +1,4 @@
-var startDate = document.querySelector('.startDate')
-var endDate = document.querySelector('.endDate')
 
-startDate.addEventListener("focusout", ()=>{
-    if (startDate.value <= endDate.value) {
-        searchAndFilter()
-    } else {
-        alert("Please enter a valid date range")
-    }
+document.getElementById('filterScorePercentage').addEventListener('change', () => {
+    updateScoreTable()
 })
-
-endDate.addEventListener("focusout", ()=>{
-    if (endDate.value >= startDate.value) {
-        searchAndFilter()
-    } else {
-        alert("Please enter a valid date range")
-    }
-})
-
-
-
-
