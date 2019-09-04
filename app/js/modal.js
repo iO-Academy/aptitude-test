42225a9c3a2b3a9ae1f9bc0c24ba1d0925c757f8
Cat <blackcatstone@gmail.com>
2019-09-04 11:33:08 +0100

    Refactor modal submission code

--- app/js/modal.js
+++ app/js/modal.js
@@ -43,3 +43,41 @@ function fillEditModalFields(HBTemplate, userInfo) {
         modal_content.innerHTML = "Please contact Admin, user list unavailable"
     }
 }
+
+/**
+ * Takes time and returns true if passed validation or false otherwise.
+ * @param time
+ * @returns {boolean}
+ */
+function isTimeValid(time) {
+    let intTime = parseInt(time)
+    return !(intTime <= 1 || intTime == null)
+}
+
+/**
+ * this is an event listener on the submit button (listens for click), 
+ * it sends the object values through our validation functions.
+ * If email exists in db then submit is not allowed. If original email 
+ * matches current or email does not exist is db then user data is updated.
+ */
+function addEditModalSubmitEventListener() {
+    document.getElementById('editSubmit').addEventListener('click', function() {
+        let name = document.getElementById("firstName").value
+        let email = document.getElementById("email").value
+        let time = document.getElementById('time').value
+        let originalEmail = document.getElementById("originalEmail").value
+        getExistingUsers().then(function (existingUsers) {
+            if (isEmpty(name) &&
+                isEmpty(email) &&
+                nameValidation(name) &&
+                isTimeValid (time) &&
+                isEmailValid(email)) {
+                if(originalEmail == email || userExists(email, existingUsers) == false){
+                    closeDialog()
+                    postUserEdit(createObjectForDatabase('.editUserData'))
+                    updateUserTable()
+                }
+            }
+        })
+    })
+}
