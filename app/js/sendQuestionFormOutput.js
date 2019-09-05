c19315c1c6f003ff340764eda329bca3b60e8799
AlisonYoon <politescarlett1@gmail.com>
2019-09-05 15:21:20 +0100

    park here to make change in addQuestion.html

--- app/js/sendQuestionFormOutput.js
+++ app/js/sendQuestionFormOutput.js
@@ -2,6 +2,13 @@ let submit = document.getElementsByClassName("submit");
 let form = document.querySelector("form");
 let formData = new FormData(form);
 
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault()
+    var questionData = {}
+    questionData.question = form.option1
+})
+
 /**
  * Convert addQuestion form data into JSON
  *
@@ -10,3 +17,43 @@ let formData = new FormData(form);
 let newQuestion = {}
 formData.forEach((value, key) => {newQuestion[key] = value})
 const newQuestionJson = JSON.stringify(newQuestion)
+
+console.log(newQuestionJson)
+/**
+ * send new question to be submitted to the db
+ *
+ * @return object - addedQuestionResponse
+ */
+// (async () => {
+//     const rawResponse = await fetch('http://localhost:8080/question', {
+//         method: 'POST',
+//         body: newQuestionJson
+//     })
+//     const content = await rawResponse.json()
+//
+//     console.log(content)
+// })();
+
+
+function sendNewQuestion(event, newQuestionJson) {
+    event.preventDefault();
+
+    let addQuestionForm = jsonToFormData(newQuestionJson)
+
+    let addedQuestionResponse = fetch("http://localhost:8080/question", {
+        method: 'post',
+        body: addQuestionForm
+    })
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(data) {
+            return data
+        })
+        .catch(function(err) {
+        })
+
+    console.log(addedQuestionResponse)
+}
+
+submit.addEventListener('click', sendNewQuestion(newQuestionJson))
