2218bfa0db142dae683527fa7fd2bd28f4f95b06
Cat <blackcatstone@gmail.com>
2019-09-05 16:46:47 +0100

    Fix

--- app/js/sendQuestionFormOutput.js
+++ app/js/sendQuestionFormOutput.js
@@ -1,3 +1,12 @@
+let authorised = false
+let user = getCookie('userEmail')
+getUser(user).then(function(user) {
+    console.log(user)
+    if ( user.data.isAdmin == '1' ) {
+        authorised = true
+    }
+})
+
 let submit = document.getElementsByClassName("submit");
 let form = document.querySelector("form");
 let formData = new FormData(form);
@@ -8,7 +17,8 @@ let formData = new FormData(form);
  */
 form.addEventListener('submit', function(event) {
     event.preventDefault()
-    if(!(user.isAdmin == "1")) {
+    if(!authorised) {
+        console.log('you\'re not alexander')
         return
     }
     var questionData = {}
@@ -32,6 +42,8 @@ function sendNewQuestion(questionData) {
     fetch("http://localhost:8080/question", {
         method: 'POST',
         body: questionData
+    }).then( response => {
+        showConfirmationMessage(response.data)
     })
 }
 
