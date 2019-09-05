747acddc64a78b0746f9994e347f030c240ab68e
Felix <felix.letheren@gmail.com>
2019-09-05 14:56:07 +0100

    Made funtion to tell how many answer fields have values.

--- app/js/newQuestionValidation.js
+++ app/js/newQuestionValidation.js
@@ -1,5 +1,26 @@
 let form = document.querySelector('form')
-document.getElementById('submit').addEventListener('click',(event) => {
+document.getElementById('submit').addEventListener('click', (event) => {
     event.preventDefault()
-    console.log(form.questionField.value)
+    formHasQuestion(form)
+    formHasBetweenOneAndFiveAnswers(form)
 })
+
+function formHasQuestion(form) {
+    if (form.questionField.value.length === 0) {
+        return false
+    } else {
+        return true
+    }
+}
+
+function formHasBetweenOneAndFiveAnswers(form) {
+    let answers = form.querySelectorAll('.answer')
+    let fieldsThatHaveValues = 0
+    answers.forEach(function(answer)
+    {
+        if(answer.value.length > 0) {
+            fieldsThatHaveValues++
+        }
+    })
+    console.log(fieldsThatHaveValues)
+}
