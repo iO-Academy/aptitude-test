8c0d839996e37198ee05281a4b87fa5423ba34f0
Felix <felix.letheren@gmail.com>
2019-09-05 15:55:15 +0100

    Added function to ensure only one checkbox is selected.

--- app/js/newQuestionValidation.js
+++ app/js/newQuestionValidation.js
@@ -17,7 +17,7 @@ function formHasQuestion(form) {
 function formHasBetweenOneAndFiveAnswers(form) {
     let answers = form.querySelectorAll('.answer')
     let fieldsThatHaveValues = 0
-    answers.forEach( (answer) => {
+    answers.forEach((answer) => {
         if (answer.value.length > 0) {
             fieldsThatHaveValues++
         }
@@ -28,14 +28,27 @@ function formHasBetweenOneAndFiveAnswers(form) {
         return false
     }
 }
+
 function answerHasValidValue(form) {
-    form.querySelectorAll('.answer-check').forEach((checkbox) =>{
-        if(checkbox.previousElementSibling.value.length > 0 && checkbox.checked === true) {
-            console.log('answer is valid')
-        }
-        else{
-            console.log('answer is invalid')
+    form.querySelectorAll('.answer-check').forEach((checkbox) => {
+        if (checkbox.previousElementSibling.value.length > 0 && checkbox.checked === true) {
+            return true
+        } else {
+            return false
         }
     })
+}
 
+function onlyOneCheckBoxIsChecked(form) {
+    let i = 0
+    form.querySelectorAll('.answer-check').forEach((checkbox) => {
+        if (checkbox.checked) {
+            i++
+        }
+    })
+    if (i === 0) {
+        return 'No answer selected'
+    } else if (i > 1) {
+        return 'Please just select one answer'
+    }
 }
