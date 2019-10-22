27b5494c5628e909cc8a80b3d02fada2d42a7c2b
John Hasznosi <noctiscreo@gmail.com>
2019-10-22 14:34:19 +0100

    Merged conflicts.

--- app/js/newTest.js
+++ app/js/newTest.js
@@ -1,23 +1,15 @@
 let testForm = document.querySelector('#testForm')
 
-testForm.addEventListener('click', function (e) {
-    let inputLength = document.querySelector('#addTestName').value.length
-    if (inputLength < 1 || inputLength > 255) {
-        alert('Test name must be less than 250 characters')
-    } 
-    e.preventDefault()
-})
-
 testForm.addEventListener('submit', function(e) {
     e.preventDefault()
-
-    if (document.querySelector('#testName').value.length > 0) {
+    let inputLength = document.querySelector('#testName').value.length
+    if (inputLength > 0 && inputLength < 256) {
         let testData = {}
         testData.name = testForm.testName.value
         testData = jsonToFormData(testData)
         const testPath = "test"
         sendData(testData, testPath)
     } else {
-        document.getElementById('message-target').innerHTML = '<p class="failure-message">Error with test input. Please try again</p>'
+        document.getElementById('message-target').innerHTML = '<p class="failure-message">Test name must be between 1 and 255 characters</p>'
     }
 })
