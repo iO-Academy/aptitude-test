983600d610ad4c0c687b3e7dc398bbee5c0ea185
Daniel Gardiner <gardiner.daniel@hotmail.co.uk>
2019-10-22 14:30:16 +0100

    updated according to Mike's comments, put dropdown in .hbs template file,

--- app/js/getTests.js
+++
@@ -1,7 +0,0 @@
-fetch('http://localhost:8080/test').then(function(data){
-    return data.json()
-}).then(function(tests){
-    let source = document.querySelector('#testTemplate').innerHTML
-    let template = Handlebars.compile(source)
-    document.querySelector('#test_id').innerHTML = template(tests)
-})
---
+++ app/js/populateTestDropdown.js
@@ -0,0 +1,9 @@
+
+async function populateTestDropdown() {
+    let availableTests = await getData('test')
+    let HBTemplate = await getTemplateAjax('js/templates/testDropdown.hbs')
+    let template = Handlebars.compile(HBTemplate)
+    document.querySelector('#test_id').innerHTML = template(availableTests)
+}
+
+populateTestDropdown()
