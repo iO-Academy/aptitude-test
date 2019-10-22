dc9aeb7754dce79c76e658e6255076a2319e2f16
cwoodwarduk <calum.james.woodward@gmail.com>
2019-10-22 11:58:46 +0100

    Fetch request to populate questionDisplay.hbs template created. Ready for code review.

--- app/js/handlebarsQuestionTable.js
+++ app/js/handlebarsQuestionTable.js
@@ -1,3 +1,24 @@
+let baseUrl = getBaseUrl()
+
+/**
+ * Fetch request to populate questionAdmin.html with questions from questions API, using questionDisplay.hbs template
+ */
+
+fetch(baseUrl + 'question')
+    .then(data => data.json())
+    .then(data => {
+
+        fetch('questionDisplay.hbs')
+            .then(template => template.text())
+            .then(template => {
+                console.log(template)
+                var hbsTemplate = Handlebars.compile(template)
+                var html = hbsTemplate(data)
+                document.querySelector('body').innerHTML += html
+            })
+
+    })
+
 /**
  * Function that will trigger a modal with the question that you selected clicking on edit button
  */
@@ -10,3 +31,4 @@ function addEditEventListeners() {
         })
     })
 }
+
