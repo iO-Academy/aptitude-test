488f6f2889617d82aa80e5f27948187fd9181d56
sudokufan <alexrichardgeorge@protonmail.com>
2019-10-22 14:29:19 +0100

    finished function

--- app/js/timeConversion.js
+++ app/js/timeConversion.js
@@ -4,29 +4,7 @@
  * @param timeInSeconds - time in seconds
  */
 function secsToMinsAndSecs(timeInSeconds) {
-    var minutes = Math.floor(timeInSeconds / 60)
-    var seconds = time - minutes * 60
-}
-
-
-
-
-function sendUserResults(number) {
-    let baseUrl = getBaseUrl()
-    let userResultsForm = jsonToFormData(userResults)
-
-    let resultsResponse = fetch(baseUrl + "answer", {
-        method: 'post',
-        body: userResultsForm
-    })
-        .then(function(response) {
-            return response.json()
-        })
-        .then(function(data) {
-            return data
-        })
-        .catch(function(err) {
-        })
-
-    return resultsResponse
+    var minutes = Math.floor(timeInSeconds / 60);
+    var seconds = timeInSeconds - (minutes * 60)
+    return [minutes, seconds]
 }
