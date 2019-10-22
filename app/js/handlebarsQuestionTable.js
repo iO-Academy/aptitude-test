4af2455ac1acfc9e85db5c36fb9e30e5938b0698
BuzatuCos <allkindcreation@gmail.com>
2019-10-22 11:15:28 +0100

    made suggested question from code review

--- app/js/handlebarsQuestionTable.js
+++ app/js/handlebarsQuestionTable.js
@@ -1,9 +1,12 @@
+/**
+ * Function that will trigger a modal with the question that you selected clicking on edit button
+ */
+
 function addEditEventListeners() {
     let editButtons = document.querySelectorAll(".modalBtn")
     editButtons.forEach(function(editButton) {
         editButton.addEventListener('click', function (e) {
             openDialog()
-
         })
     })
 }
