8d8fdddb06a6cfd2dbfa30d9b5fefbfea28cf0e1
BuzatuCos <allkindcreation@gmail.com>
2019-10-22 10:36:53 +0100

    Created modal

---
+++ app/js/handlebarsQuestionTable.js
@@ -0,0 +1,9 @@
+function addEditEventListeners() {
+    let editButtons = document.querySelectorAll(".modalBtn")
+    editButtons.forEach(function(editButton) {
+        editButton.addEventListener('click', function (e) {
+            openDialog()
+
+        })
+    })
+}
---
+++ app/js/modalQuestion.js
@@ -0,0 +1,16 @@
+/**
+ * Opens the modal dialog box.
+ */
+function openDialog() {
+    document.querySelector('#modal').style.display = 'block'
+    document.querySelector('#overlay').style.display = 'block'
+}
+
+/**
+ * Closes the modal dialog box.
+ */
+function closeDialog() {
+    document.querySelector('#modal').style.display = 'none'
+    document.querySelector('#overlay').style.display = 'none'
+}
+
