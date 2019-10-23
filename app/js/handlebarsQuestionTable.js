3c50523570787c61ff110b125f25cb82df76e1a7
BuzatuCos <allkindcreation@gmail.com>
2019-10-23 16:17:36 +0100

    Added submit functionality

--- app/js/handlebarsQuestionTable.js
+++ app/js/handlebarsQuestionTable.js
@@ -19,9 +19,32 @@ fetch(baseUrl + 'question')
                 var html = hbsTemplate(response)
                 document.querySelector('.container').innerHTML += html
                 addEditEventListeners()
+
             })
 
     })
+
+function drawTable() {
+    document.querySelector('.container').innerHTML = ''
+    fetch(baseUrl + 'question')
+        .then(data => data.json())
+        .then(response => {
+            response.data.forEach(function (question) {
+                questionsTable.data[question.id] = question;
+            })
+            fetch('js/templates/questionDisplay.hbs')
+                .then(template => template.text())
+                .then(template => {
+
+                    var hbsTemplate = Handlebars.compile(template)
+                    var html = hbsTemplate(response)
+                    document.querySelector('.container').innerHTML += html
+                    addEditEventListeners()
+
+                })
+
+        })
+}
 
 /**
  * Function that will trigger a modal with the question that you selected clicking on edit button
@@ -41,8 +64,40 @@ function addEditEventListeners() {
                             let questionAnswer = response.data.answer;
                             document.getElementById('ans' + questionAnswer).setAttribute('checked', true);
                         })
+
+                    let response = {
+                        text: null,
+                        option1: null,
+                        option2: null,
+                        option3: null,
+                        option4: null,
+                        option5: null,
+                        answer: null,
+                    }
+
+                    let answersRadio = document.querySelectorAll(".answer-check")
+                    answersRadio.forEach(function (radioButton) {
+                        radioButton.addEventListener('click', function (e) {
+                            radioButton.setAttribute('checked', true)
+                            response.answer = radioButton.getAttribute('value')
+                        })
+                    })
+
+                    document.getElementById('question-edit').addEventListener('submit', function (e) {
+                        e.preventDefault()
+
+                        response.text = document.getElementById("question-text").value
+                        response.option1 = document.getElementById("option1").value
+                        response.option2 = document.getElementById("option2").value
+                        response.option3 = document.getElementById("option3").value
+                        response.option4 = document.getElementById("option4").value
+                        response.option5 = document.getElementById("option5").value
+                        closeDialog()
+                        postQuestionEdit(response, e.target.dataset.id)
+                        drawTable()
+
+                    })
                 })
         })
     })
 }
-
