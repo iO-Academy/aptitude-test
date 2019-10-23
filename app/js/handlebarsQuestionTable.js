864862074ad386877cf0e486fcef085ecd310870
BuzatuCos <allkindcreation@gmail.com>
2019-10-23 16:53:32 +0100

    yeahhhhh boiiiiiii

--- app/js/handlebarsQuestionTable.js
+++ app/js/handlebarsQuestionTable.js
@@ -16,34 +16,15 @@ function populateQuestionTable () {
             populateHandlebarsObject('.container', 'js/templates/questionDisplay.hbs', response).then(response => {
                 let questionItems = document.querySelectorAll(".delete-question-button")
                 addDeleteQEventListeners(questionItems)
+                addEditEventListeners()
             })
-            addEditEventListeners()
+
             getQuestionCount()
         })
 }
 populateQuestionTable()
 
-function drawTable() {
-    document.querySelector('.container').innerHTML = ''
-    fetch(baseUrl + 'question')
-        .then(data => data.json())
-        .then(response => {
-            response.data.forEach(function (question) {
-                questionsTable.data[question.id] = question;
-            })
-            fetch('js/templates/questionDisplay.hbs')
-                .then(template => template.text())
-                .then(template => {
 
-                    var hbsTemplate = Handlebars.compile(template)
-                    var html = hbsTemplate(response)
-                    document.querySelector('.container').innerHTML += html
-                    addEditEventListeners()
-
-                })
-
-        })
-}
 
 /**
  * Function that will trigger a modal with the question that you selected clicking on edit button
@@ -58,46 +39,52 @@ function addEditEventListeners() {
             let handlebarsTempl = 'js/templates/editmodalquestions.hbs'
             populateHandlebarsObject('#modal', handlebarsTempl, questionsTable.data[e.target.id])
                 .then(() => {
+                    let questionAnswer = null;
                     getData('answer/' + questionsTable.data[e.target.id].id)
                         .then(response => {
-                            let questionAnswer = response.data.answer;
+                            questionAnswer = response.data.answer;
                             document.getElementById('ans' + questionAnswer).setAttribute('checked', true);
                             // populate dropdown menu with available tests
                             populateHandlebars('#test_id', 'js/templates/testDropdown.hbs', 'test')
+                        }).then(function () {
+                        let response = {
+                            text: null,
+                            option1: null,
+                            option2: null,
+                            option3: null,
+                            option4: null,
+                            option5: null,
+                            answer: questionAnswer,
+                        }
+
+                        let answersRadio = document.querySelectorAll(".answer-check")
+                        answersRadio.forEach(function (radioButton) {
+                            radioButton.addEventListener('click', function (e) {
+                                radioButton.setAttribute('checked', true)
+                                response.answer = radioButton.getAttribute('value')
+                            })
                         })
 
-                    let response = {
-                        text: null,
-                        option1: null,
-                        option2: null,
-                        option3: null,
-                        option4: null,
-                        option5: null,
-                        answer: null,
-                    }
+                        document.getElementById('question-edit').addEventListener('submit', function (e) {
+                            e.preventDefault()
+                            response.text = document.getElementById("question-text").value
+                            response.option1 = document.getElementById("option1").value
+                            response.option2 = document.getElementById("option2").value
+                            response.option3 = document.getElementById("option3").value
+                            response.option4 = document.getElementById("option4").value
+                            response.option5 = document.getElementById("option5").value
+                            closeDialog()
+                            console.log(response)
+                            console.log(e.target.dataset.id)
+                            postQuestionEdit(response, e.target.dataset.id).then((data) => {
+                                console.log(data)
+                                populateQuestionTable()
+                            })
 
-                    let answersRadio = document.querySelectorAll(".answer-check")
-                    answersRadio.forEach(function (radioButton) {
-                        radioButton.addEventListener('click', function (e) {
-                            radioButton.setAttribute('checked', true)
-                            response.answer = radioButton.getAttribute('value')
                         })
                     })
 
-                    document.getElementById('question-edit').addEventListener('submit', function (e) {
-                        e.preventDefault()
 
-                        response.text = document.getElementById("question-text").value
-                        response.option1 = document.getElementById("option1").value
-                        response.option2 = document.getElementById("option2").value
-                        response.option3 = document.getElementById("option3").value
-                        response.option4 = document.getElementById("option4").value
-                        response.option5 = document.getElementById("option5").value
-                        closeDialog()
-                        postQuestionEdit(response, e.target.dataset.id)
-                        populateQuestionTable()
-
-                    })
                 })
         })
     })
