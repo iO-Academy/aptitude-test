cb18d499b23c3a1393aaff6862b29ef61f34c65c
Anton Webb <anton25360@gmail.com>
2019-10-23 15:22:13 +0100

    Changed docblock (json to form data)

--- app/js/sendData.js
+++ app/js/sendData.js
@@ -1,11 +1,12 @@
 /**
  * Send the new/updated JSON data to the database API
  *
- * @param {JSON} data JSON data to be sent
+ * @param {FormData} data form data to be sent
  * @param {string} path The path of the API URL to send data to
  *
  * @return object Success or fail API response message
  */
+
 async function sendData(data, path) {
     let baseUrl = getBaseUrl()
     let response =  await fetch(baseUrl + path, {
