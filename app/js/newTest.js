b1e2f585c0ef98ac6bfb9137c34f752c45962d46
Mike Oram Front End Developer <mike.oram@mayden.co.uk>
2019-10-23 17:30:04 +0100

    fixed some annoying small bugs

--- app/js/newTest.js
+++ app/js/newTest.js
@@ -1,4 +1,5 @@
 let testForm = document.querySelector('#testForm')
+let responseMsg = document.querySelector('#inputSubmissionConfirmation')
 
 testForm.addEventListener('submit', async function(e) {
     e.preventDefault()
@@ -8,15 +9,18 @@ testForm.addEventListener('submit', async function(e) {
         testData.name = testForm.testName.value
         testData = jsonToFormData(testData)
         let response = await sendData(testData, 'test')
-        document.querySelector('#inputSubmissionConfirmation').innerText = response.message
+        responseMsg.innerText = response.message
         if (response.success) {
-            document.querySelector('#inputSubmissionConfirmation').className = 'alert-success'
+            responseMsg.classList.add('alert-success')
+            responseMsg.classList.remove('alert-danger')
             document.querySelector('#testName').value = ''
         } else {
-            document.querySelector('#inputSubmissionConfirmation').className = 'alert-danger'
+            responseMsg.classList.remove('alert-success')
+            responseMsg.classList.add('alert-danger')
         }
     } else {
-        document.querySelector('#inputSubmissionConfirmation').className = 'alert-danger'
-        document.querySelector('#inputSubmissionConfirmation').innerHTML = 'Test name must be between 1 and 255 characters.'
+        responseMsg.classList.remove('alert-success')
+        responseMsg.classList.add('alert-danger')
+        responseMsg.innerHTML = 'Test name must be between 1 and 255 characters.'
     }
 })
