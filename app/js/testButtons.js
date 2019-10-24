35e0814a364ead6e791cdb7d262a0cd4ed788287
James Taylor <taylor-j12@hotmail.com>
2019-10-24 12:13:44 +0100

    rearranged buttons into a span but I have lost the numbers for now

--- app/js/testButtons.js
+++ app/js/testButtons.js
@@ -2,13 +2,14 @@ let totalUsers= 413
 let usersPerPage = 20
 let buttonAmount = Math.ceil(totalUsers/usersPerPage)
 let source = document.querySelector('#paginationTemplate').innerHTML
-
+let template = Handlebars.compile(source)
+let data = {pageNo : []}
 for (let i=1; i<=buttonAmount; i++) {
-    let template = Handlebars.compile(source)
-    let data = {pageNo : i}
-    let html = template(data)
-    document.querySelector('#numberButtons').innerHTML += html
+    data.pageNo.push(i)
 }
+let html = template(data)
+document.querySelector('#numberButtons').innerHTML += html
 
 
 //populateHandlebars('#buttonBox', 'testButtons.hb', APIpath)
+//make page number an array and add each page number to it
