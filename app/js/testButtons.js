2cd0b7a7b75dee8f82b4079299d5ec16bbba9031
James Taylor <taylor-j12@hotmail.com>
2019-10-24 13:35:34 +0100

    added logic to disable buttons if you are viewing a particular page. I am unable to test it as I do not have the different pages to set it up with, but its there

--- app/js/testButtons.js
+++ app/js/testButtons.js
@@ -1,16 +1,27 @@
-let totalUsers= 413
+let totalUsers= 300
 let usersPerPage = 20
-let buttonAmount = Math.ceil(totalUsers/usersPerPage)
+
+//takes the number of users and how many users you want per page and outputs a list of numbered anchors and a prev and next button
+
+let maxPageNumber = Math.ceil(totalUsers/usersPerPage)
 let source = document.querySelector('#paginationTemplate').innerHTML
 let template = Handlebars.compile(source)
 let data = {pageNo : []}
-for (let i=1; i<=buttonAmount; i++) {
-    data.pageNo.push(String(i))
-    console.log(data.pageNo)
+for (let i=1; i<=maxPageNumber; i++) {
+    data.pageNo.push(i)
 }
 let html = template(data)
 document.querySelector('#numberButtons').innerHTML += html
 
 
 //populateHandlebars('#buttonBox', 'testButtons.hb', APIpath)
-//make page number an array and add each page number to it
+
+pageNumber = document.querySelector('.page-link').value
+
+
+if (pageNumber == 1) {
+    document.querySelector('#prev').style.disabled = disabled
+}
+else if (pageNumber == maxPageNumber) {
+    document.querySelector('#next').style.disabled = disabled
+}
