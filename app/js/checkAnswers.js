3d37af3332d2458d7c62d80485ea9847c1237a4e
markandmarkup <markeyles88@gmail.com>
2020-03-03 16:34:39 +0000

    Calc Page working. Removed getAnswered function and replaced with an answer count

--- app/js/checkAnswers.js
+++ app/js/checkAnswers.js
@@ -20,6 +20,7 @@ function finishTest() {
 async function checkAnswers(userAnswers) {
     let userScore = 0
     let answers = await getAnswers()
+
     if (answers.success) {
         answers = answers.data
         answers.forEach(function (answerItem) {
@@ -56,39 +57,15 @@ async function getAnswers() {
  */
 function getUserAnswers() {
     questionAmount = document.querySelectorAll('#questions .question').length
-    console.log(questionAmount)
     let checkedInputs = document.querySelectorAll('#questions .question .answers input:checked')
     let answers = {}
-    for (let i = 1; i <= questionAmount; i++) {
-        answers[i] = 'unanswered'
-    }
 
     checkedInputs.forEach(function(input) {
         let id = input.name.split("_")[1]
         answers[id] = input.value
     })
-    console.log(answers)
+
     return answers
-}
-
-/**
- * gets number of answered questions
- *
- * @param userAnswers answers provided by user
- * @param questionAmount total number of questions
- *
- * @return Integer number of answered questions
- */
-function getAnswered(userAnswers, questionAmount) {
-    let userAnswersArray = Object.values(userAnswers)
-    let unanswered = 0
-    userAnswersArray.forEach(function(answerItem) {
-
-            if (answerItem == "unanswered") {
-                unanswered++
-            }
-        })
-    return questionAmount - unanswered
 }
 
 /**
@@ -159,7 +136,7 @@ function showResults() {
             document.querySelector('#question_page').style.display = 'none'
             document.querySelector('#result_page').style.display = 'block'
             percentResult = getPercentResult(result.score, questionAmount)
-            answered = getAnswered(userAnswers, questionAmount)
+            answered = document.querySelectorAll('#questions .question .answers input:checked').length
             displayResult(result.score, percentResult, answered)
             handleResponseFromAPI(sendUserResults(result))
         } else {
