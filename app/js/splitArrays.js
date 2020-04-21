b850e01de1912bf5ae8b8b362d3aa40398fef2af
MaxG <mh.granger@yahoo.co.uk>
2020-04-21 14:25:57 +0100

    Updated documentation

--- app/js/splitArrays.js
+++ app/js/splitArrays.js
@@ -1,10 +1,10 @@
 /**
- * Breaks down an Array into arrays of specified sizes and an array of the remaining values.
+ * Breaks down the input into smaller arrays of specified size and an array of the remaining values.
  *
  * @param initialArray is the original array you want broken down
  * @param newArrayLength is the length you want the smaller arrays to be
  *
- * @returns An array of the smaller arrays
+ * @returns A multi-dimensional array of the smaller arrays
  */
 function splitArray (initialArray, newArrayLength) {
     const numOfNewArrays = Math.ceil(initialArray.length / newArrayLength);
