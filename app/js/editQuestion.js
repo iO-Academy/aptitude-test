cc629fe0a0fda06dcbc727b24eb869a38b19455c
turnerchristian <christianturner@hotmail.co.uk>
2020-04-22 14:08:38 +0100

    Added sending a question to API into a function to be reused for both addQuestion and editQuestion pages.

--- app/js/editQuestion.js
+++ app/js/editQuestion.js
@@ -39,7 +39,7 @@ async function getAnswerByQuestionId(id) {
     const [question] = await getQuestionById(questionId);
     const answer = await getAnswerByQuestionId(questionId);
 
-    document.querySelector("#question-to-edit").value = question.text;
+    document.querySelector("#questionText").value = question.text;
     document.querySelector("#option1").value = question.option1;
     document.querySelector("#option2").value = question.option2;
     document.querySelector("#option3").value = question.option3;
@@ -49,48 +49,9 @@ async function getAnswerByQuestionId(id) {
     document.querySelector(`input[data-question-id="${answer}"]`).checked = 1;
 
     form.addEventListener("submit", async (event) => {
-        event.preventDefault();
-
-        const responseMsg = document.querySelector('#inputSubmissionConfirmation');
-        const hasQuestions = formHasQuestion(form);
-        const hasAnswers = formHasBetweenTwoAndFiveAnswers(form);
-        const isValid = answerHasValidValue(form);
-
-        if (hasQuestions && hasAnswers && isValid) {
-            let answer;
-
-            document.querySelectorAll('.answer-check').forEach((answerCheck, index) => {
-                if (answerCheck.checked) {
-                    answer = index + 1;
-                }
-            });
-
-            const {success, message} = await editQuestion(questionId, {
-                text: document.querySelector("#question-to-edit").value,
-                option1: document.querySelector("#option1").value,
-                option2: document.querySelector("#option2").value,
-                option3: document.querySelector("#option3").value,
-                option4: document.querySelector("#option4").value,
-                option5: document.querySelector("#option5").value,
-                test_id: document.querySelector("#test_id").value,
-                answer
-            });
-
-            responseMsg.textContent = message;
-
-            if (success) {
-                responseMsg.classList.add('alert-success');
-                responseMsg.classList.remove('alert-danger');
-            } else {
-                responseMsg.classList.remove('alert-success');
-                responseMsg.classList.add('alert-danger');
-            }
-        } else {
-            responseMsg.classList.remove('alert-success');
-            responseMsg.classList.add('alert-danger');
-            responseMsg.textContent = 'Error: Please ensure you have filled out the question form correctly.';
-        }
+        await sendQuestionForm(event, form, true, questionId);
     });
 })();
+
 
 
