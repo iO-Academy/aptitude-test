fbc8fffdf2612e35146fe2660badf21f74d695ad
HiredMussel <alexmusselwhite2@gmail.com>
2020-04-22 17:02:11 +0100

    do not display test on graph if no users have taken it

--- app/js/sendToChart.js
+++ app/js/sendToChart.js
@@ -2,12 +2,15 @@
  * function to generate the data object for the chart displayed on the front end.
  * @returns the data object for graph.js
  */
-async function sendToChart() {   
+async function sendToChart() { 
+    let users = await createUsersObject();
+    users = searchAndFilter(users.data);
+    
     let dataForChart = {};
-    let labels = await generateLabels();
-    let dubiousGrade = await generateDataset("Under 70%", labels, 0, 70, "#f27324");
-    let passingGrade = await generateDataset("70% to 97%", labels,  70, 97, "#94ba66");
-    let topGrade = await generateDataset("97% and above", labels, 97, 101, "#d2b4f9");
+    let labels = await generateLabels(users);
+    let dubiousGrade = await generateDataset("Under 70%", labels, users, 0, 70, "#f27324");
+    let passingGrade = await generateDataset("70% to 97%", labels, users,  70, 97, "#94ba66");
+    let topGrade = await generateDataset("97% and above", labels, users, 97, 101, "#d2b4f9");
 
     dataForChart.labels = labels;
     dataForChart.datasets = [dubiousGrade, passingGrade, topGrade];
@@ -19,6 +22,7 @@ async function sendToChart() {
  * 
  * @param {datasetLabel} string the label for the dataset
  * @param {testLabels} Array the array of strings containing the names of the tests
+ * @param {users} Object the object containing information about all current users. Comes from createUsersObject
  * @param {minPercentage} number the least percentage to be included in the range, inclusive
  * @param {maxPercentage} number the greatest percentage to be included in the range, exclusive
  * @param {color} string the colour code for entries in this dataset
@@ -26,11 +30,9 @@ async function sendToChart() {
  * @return Object a dataset object containing: A label, an array of data, and an array of background
  *                colors for that data (all set to the same value, passed in as color) 
  */
-async function generateDataset(datasetLabel, testLabels, minPercentage, maxPercentage, color) {
+async function generateDataset(datasetLabel, testLabels, users, minPercentage, maxPercentage, color) {
 
     let dataset = {};
-    let users = await createUsersObject();
-    users = searchAndFilter(users.data);
     dataset.data = [];
     dataset.label = datasetLabel;
     dataset.backgroundColor = [];
@@ -61,18 +63,26 @@ async function generateDataset(datasetLabel, testLabels, minPercentage, maxPerce
 /**
  * function to generate the array of names of the tests as labels for the data object
  * 
+ * @param {users} Object object containing user information
+ * 
  * @return Array the array of strings containing the names of all the tests registered
  *         in the database
  */
-async function generateLabels() {
+async function generateLabels(users) {
 
     let tests = await getTests();
     let labels =[];
 
     tests.forEach(function (test) {
-
-        labels.push(test.name);
-
+        let anyUsers = false;
+        users.forEach(function(user) {
+            if (user.testAllocated == test.name && !user.testNotTaken) {
+                anyUsers = true;
+            }
+        })
+        if (anyUsers) {
+            labels.push(test.name);
+        }
     });
 
     return labels;
