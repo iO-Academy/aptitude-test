0fe75a5f244dba522fff4e0dfb6c49625c79cfd8
Alex Jennings <jenningsalexyt@gmail.com>
2020-04-23 15:29:10 +0100

    changed indentation and added ;

--- app/js/displayPageButtons.js
+++ app/js/displayPageButtons.js
@@ -13,33 +13,37 @@ async function displayPageBtns(paginatedArray) {
 function pageSelectorFunctionality(HBTemplate, paginatedArrays) {
     const pages = paginatedArrays.length;
     let page = 1;
+
     pageButtonCheck(page, pages);
+
     document.querySelectorAll('.pageBtn').forEach((btn) => {
-        btn.addEventListener('click', (evt => {
+        btn.addEventListener('click', ((evt) => {
             if (page > 0 && page <= pages) {
                 if (Number.isInteger(parseInt(btn.dataset.page))) {
-                    page = btn.dataset.page
+                    page = parseInt(btn.dataset.page);
                 } else if ((btn.dataset.page === 'next') && (page < pages)) {
                     page++;
                 } else if ((btn.dataset.page === 'back') && (page > 1)) {
                     page--;
                 }
             }
-            printFilteredResultsToScreen(HBTemplate, paginatedArrays[page - 1])
+
+            printFilteredResultsToScreen(HBTemplate, paginatedArrays[page - 1]);
             pageButtonCheck(page, pages);
         }));
     });
 }
 
-function pageButtonCheck(page, pages){
-    if (page == '1') {
+function pageButtonCheck(page, pages) {
+    if (page === 1) {
         document.querySelector('.backBtn').classList.add('greyedOutBtn');
-        } else {
-            document.querySelector('.backBtn').classList.remove('greyedOutBtn');
-        }
-        if (parseInt (page) == pages) {
-            document.querySelector('.nextBtn').classList.add('greyedOutBtn');
-        } else {
-            document.querySelector('.nextBtn').classList.remove('greyedOutBtn');
-        }
+    } else {
+        document.querySelector('.backBtn').classList.remove('greyedOutBtn');
+    }
+
+    if (page === pages) {
+        document.querySelector('.nextBtn').classList.add('greyedOutBtn');
+    } else {
+        document.querySelector('.nextBtn').classList.remove('greyedOutBtn');
+    }
 }
