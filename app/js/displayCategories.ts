e1accb1999a69c18d4facf62dae9d134bb5ab738
Davin <davin.hayden@outlook.com>
2020-10-27 13:22:12 +0000

    renamed helper function from ifEquals to ifNotEquals in displayCategories.ts and updated where its called from in categoryItem.hbs, and added better comments to displayCategories.ts

--- app/js/displayCategories.ts
+++ app/js/displayCategories.ts
@@ -1,15 +1,15 @@
 /**
  * Compares 2 values and allows us to check a conditional statement in HBS
  *
- * @param arg1 first item to compare
- * @param arg2 second item to compare against first item
+ * @param arg1 first item to compare, the id of the current category from the API
+ * @param arg2 second item to compare against first item, the value of 1 which is the id that doesnt need a Delete button
  * @param opts - option to run the code inside result of 'IF' statement on  line 5 categoryItems.hbs
  */
-Handlebars.registerHelper('ifEquals', function(arg1, arg2, opts) {
+Handlebars.registerHelper('ifNotEquals', function(arg1, arg2, options) {
     if (arg1 != arg2) {
-        return opts.fn(this); //this run code inside result of if stmt - line 5 categoryItems.hbs
+        return options.fn(this); //this run the code inside the result of the 'if' stmt in line 5 categoryItems.hbs
     } else {
-        return opts.inverse(this);
+        return options.inverse(this);
     }
 });
 
