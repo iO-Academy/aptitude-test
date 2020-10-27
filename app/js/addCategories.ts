64319870f74d8a6a22f5b8ec75d709f677c4146f
El Stevens <eleanorkwstevens@gmail.com>
2020-10-27 11:15:22 +0000

    added semicolons, removed unnecessary true boolean, indented else statement content

--- app/js/addCategories.ts
+++ app/js/addCategories.ts
@@ -1,29 +1,29 @@
 document.querySelector<HTMLFormElement>('.categoriesForm').addEventListener('submit', (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     let newCategory: string = document.querySelector<HTMLFormElement>('#categoryName').value;
-    let responseMessage = document.querySelector('#addedCategoryConfirmation')
+    let responseMessage = document.querySelector('#addedCategoryConfirmation');
 
     if (newCategory.length > 255) {
-        responseMessage.textContent = 'Error - Category Name Too Long.'
+        responseMessage.textContent = 'Error - Category Name Too Long.';
     } else {
 
-    let data: object = {"name": newCategory};
-    let dataToSend: FormData = jsonToFormData(data);
-    document.querySelector<HTMLFormElement>('#categoryName').value = '';
+        let data: object = {"name": newCategory};
+        let dataToSend: FormData = jsonToFormData(data);
+        document.querySelector<HTMLFormElement>('#categoryName').value = '';
 
-    sendData(dataToSend, 'category').then((data) => {
+        sendData(dataToSend, 'category').then((data) => {
 
-        if (data.success === true){
-            responseMessage.textContent = 'Success! Category added';
-            responseMessage.classList.remove('alert-danger');
-            responseMessage.classList.add('alert-success');
+            if (data.success) {
+                responseMessage.textContent = 'Success! Category added';
+                responseMessage.classList.remove('alert-danger');
+                responseMessage.classList.add('alert-success');
 
-        } else {
-            responseMessage.textContent = 'Error - Category Not Added';
-            responseMessage.classList.add('alert-danger');
-            responseMessage.classList.remove('alert-success');
-        }
-    });
+            } else {
+                responseMessage.textContent = 'Error - Category Not Added';
+                responseMessage.classList.add('alert-danger');
+                responseMessage.classList.remove('alert-success');
+            }
+        });
     }
 });
