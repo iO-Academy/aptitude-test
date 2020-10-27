31b5016d107b1726e1b65dc1f63fbbb4c2012df6
Davin <davin.hayden@outlook.com>
2020-10-27 12:01:58 +0000

    added HB helper to displayCategories.ts and adjusted categoryItem.hbs to only output Delete button if Category is not Student

--- app/js/displayCategories.ts
+++ app/js/displayCategories.ts
@@ -1,20 +1,17 @@
-// function to get categories via api
-async function displayCategories() {
-    try {
-        let dataObj = await fetch('localhost:8080/category');
-        let categories = await dataObj.json();
+/**
+ * Compares 2 values and allows us to check a conditional statement in HBS
+ *
+ * @param arg1 first item to compare
+ * @param arg2 second item to compare against first item
+ * @param opts - option to run the code inside result of 'IF' statement on  line 5 categoryItems.hbs
+ */
+Handlebars.registerHelper('ifEquals', function(arg1, arg2, opts) {
+    if (arg1 != arg2) {
+        return opts.fn(this); //this run code inside result of if stmt - line 5 categoryItems.hbs
+    } else {
+        return opts.inverse(this);
     }
-    catch(error) {
-        console.log(`couldnt fetch ${error}`);
-    }
+});
 
-    // console.log(categories);
-    // let templateHBS = await fetch('templates/categoryItems.hbs');
-    // var template = Handlebars.compile(templateHBS);
-    // // document.querySelector('#categoriesContainer').innerHTML;
-    // categories.data.forEach(category => {
-    //     document.querySelector('#categoriesContainer').innerHTML += "blah"; // template(category);
-    // })
-}
+populateHandlebars('#categoriesContainer', 'js/templates/categoryItem.hbs', 'category');
 
-displayCategories();
