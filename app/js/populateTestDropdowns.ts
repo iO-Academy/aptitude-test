1d6d589140a8c41bc533bfd4ba44de92a1ad4436
Kashif Ameen <kashu@live.co.uk>
2021-04-27 14:42:05 +0100

    removed excess code

--- app/js/populateTestDropdowns.ts
+++ app/js/populateTestDropdowns.ts
@@ -14,11 +14,7 @@ function populateTestDropdowns () {
         getTemplateAjax('js/templates/testDropdown.hbs').then((HBTemplate) => {
             let template: Function = Handlebars.compile(HBTemplate);
             document.querySelector<HTMLElement>('#test_id').innerHTML = template(testsObject);
-
-            document.querySelector<HTMLElement>('#linkToEditTestsBtn').setAttribute('href', `editTests.html#${testsObject.data[0].id}`);
-
-
-            document.querySelectorAll(".test_options").forEach((test_option: HTMLInputElement) => {
+              document.querySelectorAll(".test_options").forEach((test_option: HTMLInputElement) => {
                 populateUserDuration(test_option, testFieldValue);
             });
         });
