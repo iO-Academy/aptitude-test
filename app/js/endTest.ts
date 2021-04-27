f2e1ba3c2b4154ca289d6ea8e449c6e588f5e663
Isabelle Cochrane <isabelle_cochrane@hotmail.co.uk>
2021-04-27 09:37:36 +0100

    timeout for alert

--- app/js/endTest.ts
+++ app/js/endTest.ts
@@ -1,16 +1,22 @@
 let count: number = 0;
+let alertActive: boolean = true;
+
 
 function pageLeaveAlert() {
-    if (count < 2) {
+    if (count < 2 && alertActive) {
         count ++;
         alert ('WARNING, stay in this window or you will fail the test!')
+        alertActive = false;
+        setTimeout(()=> {
+            alertActive = true;
+        }, 5000)
     }
 }
 
 function cancelTest() {
     if (document.visibilityState === "hidden") {
-        let pageLeft: boolean = true
-        finishTest(pageLeft)
+        let pageLeft: boolean = true;
+        finishTest(pageLeft);
     }
 }
 
