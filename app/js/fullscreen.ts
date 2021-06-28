f3c089b7c8b19339be154b7c6c097d59079e4ab7
Saskia <saskia_west1996@hotmail.co.uk>
2021-06-28 13:55:57 +0100

    add scss class hide full screen button + ts to remove button when incompatible

--- app/js/fullscreen.ts
+++ app/js/fullscreen.ts
@@ -1,9 +1,16 @@
+if (!(document.fullscreenEnabled)) {
+    document.getElementById('toggle-fullscreen').classList.add('hide-toggle-fullscreen');
+    console.log(document.fullscreenEnabled)
+}
 
 const fullscreenButton = document.querySelector('#toggle-fullscreen');
 fullscreenButton.addEventListener('click', toggleFullscreen);
+
 
 function toggleFullscreen() {
     if (!document.fullscreen) {
         document.documentElement.requestFullscreen();
     } document.exitFullscreen()
 }
+
+
