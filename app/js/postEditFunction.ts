03c8caacf02cb59e2dc898945c56ce526d4d2ac9
SniffleSneeze <70772425+SniffleSneeze@users.noreply.github.com>
2021-06-30 14:18:38 +0100

    added tick box for user to resum test / add object canResum insid function postEditFunction.ts

--- app/js/postEditFunction.ts
+++ app/js/postEditFunction.ts
@@ -10,11 +10,15 @@ import {BaseUser} from "./interfaces/User";
  */
 function createObjectForDatabase(inputClass: string) {
     let formData = document.querySelectorAll(inputClass);
-    let result = {'canRetake': (document.querySelector<HTMLInputElement>('#canRetake').checked ? 1 : 0)}
+    let result = {
+        'canRetake': (document.querySelector<HTMLInputElement>('#canRetake').checked ? 1 : 0),
+        'canResume': (document.querySelector<HTMLInputElement>('#canResume').checked ? 1 : 0)
+    }
 
     formData.forEach(function (input: HTMLInputElement) {
         result[input.name] = input.value;
     });
+    console.log('where the fuck are you')
     return result
 }
 
