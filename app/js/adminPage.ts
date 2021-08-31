87a807f5cbfb780f3ff0471ca2e310906646b4af
Luke Landau <luke.a.landau@gmail.com>
2021-08-31 16:50:29 +0100

    Linked checkbox/submit with database

--- app/js/adminPage.ts
+++ app/js/adminPage.ts
@@ -23,6 +23,8 @@ async function saveNewUser(user: BaseUser) {
         }
     );
 
+    console.log(formData)
+
     let apiData = await apiResponse.json();
     return apiData;
 }
@@ -98,6 +100,7 @@ document.querySelector('#addNewUserForm').addEventListener('submit', function(ev
     let timeMinutes = document.querySelector<HTMLInputElement>('#user_time_minutes').value;
     let timeSeconds = document.querySelector<HTMLInputElement>('#user_time_seconds').value;
     let timeTotal = convertToTotalTimeSeconds(timeMinutes, timeSeconds);
+    let timerIsShown =  document.querySelector<HTMLInputElement>('#timer').checked ? 1 : 0;
 
     getExistingUsers().then(function(existingUsers) {
         if (!isEmailValid(emailField.value) || userExists(emailField.value, existingUsers)) {
@@ -117,7 +120,8 @@ document.querySelector('#addNewUserForm').addEventListener('submit', function(ev
                 email: emailField.value,
                 test_id: testField.value,
                 category_id: categoryField.value,
-                time: timeTotal
+                time: timeTotal,
+                showTimer: timerIsShown
             }).then(function(response) {
                 if (response.success) {
                     errorField.classList.add('alert-success');
